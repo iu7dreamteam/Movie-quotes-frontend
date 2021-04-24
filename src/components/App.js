670992c82c4921e02bef85036635d9667f14527c
SanSanchezzz <13lex04@gmail.com>
2021-04-24 22:21:19 +0300

    MQ-24: Configure libs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import "../styles/App.css"
+import "../styles/App.css";
 
 class App extends React.Component {
     render () {
         return (
-            <div>
-                <h1>Movie quotes</h1>
-            </div>
+            <Router>
+                <div>
+                    <h1>Movie quotes</h1>
+                    <Switch>
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+// export default combineReducers({
+//
+// });
+
+export default function(state = {}, action) { return state; }
